b9cca9912551a39281373081778c636e20aa3169
Garet Hough <garetshough@gmail.com>
2021-07-05 22:22:59 -0700

    Finished the treasure hunting bot which is based off of common to rare items you get based off of roll number between 1 and 10000

--- Discord Bots/Treasure_trail_bot/app.js
+++ Discord Bots/Treasure_trail_bot/app.js
@@ -25,17 +25,56 @@ client.on("message", message =>{
     // random reward command
     if (inputToLowerCase.includes("!randomreward") && message.author.bot == false){
         
+        // array of strings
         var decoded_message = inputToLowerCase.split(" ");
 
+        // amount of treasure chests before flooring the number
         var correct_number = Number(decoded_message[1]);
 
+        //amount of treasure chests after flooring the number
         var floored_number = Math.floor(correct_number);
 
-        if (Number(floored_number)){
-            console.log("true");
+        // 1-10000
+        if (decoded_message[0] == "!randomreward" && Number(floored_number && decoded_message.length == 2 && floored_number > 0 && floored_number < 10001)){
+            // variable holding all the item rolls
+            var reward_rolls = 0;
+
+            // total amount of rare rewards
+            var total_ranger_boots = 0;
+            var total_wizard_boots = 0;
+            var total_holy_boots = 0;
+
+            // for loop determining amount of rolls
+            for (var i = 0; i < floored_number; i++){
+                var rolls_this_chest = Math.floor(Math.random() * 3) + 3;
+                reward_rolls += rolls_this_chest;
+            }
+
+            for (var i = 0; i < reward_rolls; i++){
+                var reward_roll = Math.floor(Math.random() * 1000) + 1;
+                
+                if (reward_roll == 100){
+                    total_ranger_boots++;
+                }
+
+                else if (reward_roll == 200){
+                    total_wizard_boots++;
+                }
+
+                else if (reward_roll == 300){
+                    total_holy_boots++;
+                }
+            }
+
+            console.log("Ranger boots: " + total_ranger_boots);
+            console.log("Wizard boots: " + total_wizard_boots);
+            console.log("Holy boots: " + total_holy_boots);
+
+            message.reply("\n" + "You recieved the following rewards based off of your number of treasure chests: " + floored_number + "\n" + "ranger boots: " + total_ranger_boots + "\n" + "wizard boots: " + total_wizard_boots + "\n" + "holy boots: " + total_holy_boots);
         }
+        // wrong input
         else {
-            console.log("false");
+            console.log("wrong input");
         }
     }
 
