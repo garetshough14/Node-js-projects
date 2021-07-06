730e8d286f3104126f10624d7ed180e0b8591ca7
Garet Hough <garetshough@gmail.com>
2021-07-05 23:51:23 -0700

    started making the event date bot

--- Discord Bots/Event-Date-Bot/app.js
+++ Discord Bots/Event-Date-Bot/app.js
@@ -4,6 +4,16 @@ const Discord = require("discord.js");
 // Create a new client using the new keyword
 const client = new Discord.Client();
 
+// array of commands
+const commandsArray = ["!commands", "!event", "!author"];
+
+// reply array
+const replyArray = [
+    "The owner is Garet Hough, Garet is the owner of Event Date Bot",
+    "The owner is Garet Hough, Erik is the owner of Event Date Bot",
+    "The owner is Garet Hough, Erik is the owner of Event Date Bot"
+];
+
 // require password from external file
 const {
     token
@@ -15,12 +25,18 @@ client.on("ready", () =>{
 });
 
 // Check messages for a specific command
-client.on("message", msg =>{
-    // Send back a reply when the specific command has been written
-    if (msg.content === "!hello"){
-        msg.reply("Hello world!");
+client.on("message", async msg =>{
+    // Check for !commands
+    if (msg.content === commandsArray[0]){
+        // Displays all commands
+        msg.reply("This bot's commands are !commands !event !author");
+    }
+
+    // Check for !author
+    else if (msg.content === commandsArray[2]){
+        // send back a message about the author
+        msg.reply(replyArray[0] + replyArray[1] + replyArray[2]);
     }
 });
-
 // Log in the bot with the token
-client.login("token");
+client.login(token);
